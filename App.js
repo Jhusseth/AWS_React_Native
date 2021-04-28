604db4e57e5600b2448ac227c89ad30445e0b361
Jhusseth <jhusseth.sanchez@u.icesi.edu.co>
2021-04-27 19:11:15 -0500

    add main panel called Home and add navigations in screens of verification and Register

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TouchableHighlight, StyleSheet} from 'react-native';
+import Registration from './components/auth/Registration';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Verification from './components/auth/Verification';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class HomeScreen extends React.Component {
+   render() {
+       return (
+           <View style={{ flex: 1, alignItems: "center" }}>
+               <Text style= {{ fontSize: 30, color: "#000", marginBottom: 50, marginTop: 100 }}>Register Face ID</Text>
+               <TouchableHighlight style={[styles.buttonContainer, styles.button]} onPress={() => this.props.navigation.navigate('Registration')}>
+                   <Text style={styles.buttonText}>Registration</Text>
+               </TouchableHighlight>
+               <TouchableHighlight style={[styles.buttonContainer, styles.button]} onPress={() => this.props.navigation.navigate('Verification')}>
+                   <Text style={styles.buttonText}>Verification</Text>
+               </TouchableHighlight>
+           </View>
+       );
+   }
+}
+
+const MainNavigator = createStackNavigator(
+   {
+       Home: {screen: HomeScreen},
+       Registration: {screen: Registration},
+       Verification: {screen: Verification}
+   },
+   {
+       initialRouteName: 'Home',
+   }
+);
+
+const AppContainer = createAppContainer(MainNavigator);
+
+export default class App extends Component {
+   render() {
+       return <AppContainer />;
+   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   buttonContainer: {
+       height:45,
+       flexDirection: 'row',
+       alignItems: 'center',
+       justifyContent: 'center',
+       marginBottom:20,
+       width:"80%",
+       borderRadius:30,
+       marginTop: 20,
+       marginLeft: 5,
+   },
+   button: {
+       backgroundColor: "#337ab7",
+       width: 350,
+       marginTop: 5,
+   },
+   buttonText: {
+       color: 'white',
+       fontWeight: 'bold',
+   },
+})
